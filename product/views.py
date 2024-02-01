589d4f94876edf4369011a3feb273bbc8e863f43
Nasirudd-in <nasir.diudevs@gmail.com>
2024-02-01 15:24:31 +0600

    product details added

--- product/views.py
+++ product/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
+    
+class ProductDetails(TemplateView):
+    template_name = 'product/product-details.html'

ff5cf45fb127c2660f9801574eceb902ca13f0ad
nasirudd-in <nasir.diudevs@gmail.com>
2024-02-02 22:39:03 +0600

    featured product and latest product added

--- env/bin/activate.csh
+++ env/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/bdtask/dj_ecom_website/env"
+setenv VIRTUAL_ENV "/home/nasir/Desktop/DJ-ECOMMERCE/env"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
--- product/views.py
+++ product/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from .models import(
+    Category,
+    Product,
+    Slider
+)
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
-    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {
+                'featured_categories': Category.objects.filter(featured=True),
+                'featured_products': Product.objects.filter(featured=True),
+                'sliders': Slider.objects.filter(show=True)
+            }
+        )
+        return context
+        
 class ProductDetails(TemplateView):
     template_name = 'product/product-details.html'

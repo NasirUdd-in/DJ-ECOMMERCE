37ceb65878b5ccbc4ad1c4f6cdb0b06047f5bd67
nasirudd-in <nasir.diudevs@gmail.com>
2024-02-04 22:41:00 +0600

    checkout form submit and only error check in the console

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('', include('product.urls')),
     path('',include('user_account.urls')),
     path('',include('cart.urls')),
+    path('',include('order.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

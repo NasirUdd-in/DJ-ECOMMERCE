8ca849f906abb6aed1cfc79a22f196984a799f14
abdurrazzak0096 <abdurrazzak0096@gmail.com>
2024-02-05 07:26:13 -0500

    home image added

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nasir/Desktop/DJ-ECOMMERCE/env"
+set -gx VIRTUAL_ENV "/home/bdtask/Documents/DJ-ECOMMERCE/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

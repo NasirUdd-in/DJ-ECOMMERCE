b35f1cdbd29fc9185614e84e31f9c13ac3ca2c15
Nasirudd-in <nasir.diudevs@gmail.com>
2024-02-12 12:25:20 +0600

    search updated

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rv1k8_qc_&apq8zh9&oi&+$^0^@_tk953)1osv9ksn9skib89u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.181']
+ALLOWED_HOSTS = ['192.168.1.181', '127.0.0.1']
 
 
 # Application definition

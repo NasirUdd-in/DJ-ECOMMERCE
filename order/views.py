12e2bed2aac54dcd8110bdfbe285e4e71aab2334
Nasir Uddin <86841234+NasirUdd-in@users.noreply.github.com>
2024-02-19 11:12:34 +0600

    Order updated

--- order/views.py
+++ order/views.py
@@ -18,6 +18,9 @@ from .models import OrderItem
 from django.db.models import Sum
 from .forms import DateRangeForm
 
+
+
+
 class Checkout (LoginRequiredMixin, generic.View):
      login_url = reverse_lazy('login')
      def get(self, *args, **kwargs):
@@ -26,24 +29,24 @@ class Checkout (LoginRequiredMixin, generic.View):
             'form': form
         }
         return render(self.request, 'order/checkout.html', context)
-     
+
      def post(self, *args, **kwargs):
         form = CheckoutForm(self.request.POST)
-        
+
         if form.is_valid():
             data = form.cleaned_data
             print(data)
             return JsonResponse({
                 'success': True,
                 "errors": None
-                                    
+
             })
         else:
             return JsonResponse({
                 'success': False,
                 "errors": dict(form.errors)
             })
-            
+
 
 class SaveOrder (LoginRequiredMixin, generic.View):
     login_url = reverse_lazy('login')
@@ -60,30 +63,30 @@ class SaveOrder (LoginRequiredMixin, generic.View):
                 quantity=user_cart[str(product.id)]['quantity']
             )
             ordered_products.append(order_item)
-            
+
         order = Order.objects.create(
             user = self.request.user,
             transaction_id=uuid.uuid4().hex,
             **customer_information
         )
-        
+
         order.order_items.add(*ordered_products)
-        
+
         #security puerpose
         if float('%.2f' % cart.total()) != float(order.total):
             order.paid = False
             order.save()
-         
+
         cart.clear()
         return JsonResponse({'success': True})
-    
-    
+
+
 class Orders(LoginRequiredMixin, generic.ListView):
         login_url = reverse_lazy( 'login')
         model = Order
         template_name = 'order/orders.html'
         context_object_name = 'orders'
-        
+
         def get_queryset(self):
             return Order.objects.filter(user=self.request.user)
 
@@ -99,7 +102,7 @@ class Orders(LoginRequiredMixin, generic.ListView):
 #         # If the user is a normal user, show only their sold products
 #         seller_orders = OrderItem.objects.filter(product__seller=request.user)
 #     total_price = seller_orders.aggregate(Sum('price'))['price__sum'] or 0
-    
+
 #     return render(request, 'seller/seller_dashboard.html', {'seller_orders': seller_orders, 'total_price': total_price})
 
 # views.py
@@ -112,7 +115,7 @@ from .models import OrderItem, Product
 @login_required
 def seller_dashboard(request):
     date_range_form = DateRangeForm(request.GET)
-    
+
     if request.user.is_staff:
         # If the user is an admin, show all sold products
         seller_orders = OrderItem.objects.all()
